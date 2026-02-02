ac170f44e1cdd415fff7a62ca73ab2ed77ef767d
PavelSheremetev <spd@aira.life>
2026-02-02 21:44:31 +0200

    FIX vars

--- letsencrypt/rootfs/etc/cont-init.d/99-proxy-settings.sh
+++ letsencrypt/rootfs/etc/cont-init.d/99-proxy-settings.sh
@@ -5,13 +5,11 @@
 
 if bashio::config.has_value 'http_proxy'; then
     PROXY=$(bashio::config 'http_proxy')
-    bashio::log.info "Setting Proxy: ${PROXY}"
-    
-    export http_proxy="${PROXY}"
-    export https_proxy="${PROXY}"
-    export HTTP_PROXY="${PROXY}"
-    export HTTPS_PROXY="${PROXY}"
-    
-    export NO_PROXY="localhost,127.0.0.1,local.hass.io"
+    bashio::log.info "Pre-start: Proxy detected: ${PROXY}"
+    # Здесь мы не можем просто сделать export, так как это другой процесс.
+    # Поэтому мы запишем прокси в системный файл окружения.
+    echo "http_proxy=${PROXY}" >> /etc/environment
+    echo "https_proxy=${PROXY}" >> /etc/environment
+    echo "HTTP_PROXY=${PROXY}" >> /etc/environment
+    echo "HTTPS_PROXY=${PROXY}" >> /etc/environment
 fi
-
